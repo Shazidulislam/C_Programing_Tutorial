11ab6402ef4725b436e3cec3654cf38b26c1a857
Your Name <you@example.com>
2025-12-23 20:25:51 +0600

    learn variable type

--- Hello.c
+++ Hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     printf("Hello World");
     // printf("How are you! Bro Kemon Asen!");
     int number = 25;
@@ -9,13 +10,14 @@ int main(){
     age = 22;
     float pi = 3.14;
     // variable ar case sensetive
-     int a = 30;
-     int A = 40;
-     
+    int a = 30;
+    int A = 40;
+
     //  you  can start write variable only use alpabet and _
     // first
     int _age = 22;
     // !second
+    // *** c programin e string nai
     int start = "string";
     // varable maje kono khali jaiga rakha jabe na
     // tobe mjkhane _ (hig pen use korte parbe
@@ -35,9 +37,68 @@ int main(){
 // !
 // !d. No other symbol other than '_'
 
-
-
-
-
 // ?Variables
 // ?Data Types
+
+int main()
+{
+    //! int er moddhe interjer valu rakhbo, mane intijer joto value ta sob int data type rakbo
+    int gae = 20;
+
+    // ?decimal valu store korbo float data type e
+    float PI = 3.1416;
+
+    // !! Character typer joto value/ ase ta sob store korbo char data type e
+    char hashtar  = "#";
+
+
+    return 0;
+}
+
+// **Constants  //!! Constants mane je ei variable jei value rakba oi change kora jabe na
+
+// **Values that don't change(fixed)
+
+// !!  Types const e types 3 doroner hoi
+
+// ?? 1. Ineger Constants --> 1,2, 0 , -1 , -2
+//??  2. Real Constants   --> 1.0 , 2.0 , 3.23 , 
+//??  3. Character        --> 'A' , 'a' , '%'  , '#'
+
+// !! Note Character ke single cots '' use korte hobe
+
+
+
+
+
+
+
+
+
+
+
+
+// ** 
+//!! Keywords
+// ?Reserved words that have special
+// ?meaning to the compiler
+// */
+
+//**Keywords */
+// ** auto double int struct
+// ** 
+// ** break else long switch
+// ** 
+// ** case enum register typedef
+// ** 
+// ** char extern return union
+// ** 
+// ** continue for signed void
+// ** 
+// ** do if static while
+// ** 
+// ** default goto sizeof volatile
+// ** 
+// ** const float short unsigned
+
+//! ei keywords gula variable hisabe likte parbo na

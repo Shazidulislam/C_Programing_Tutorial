01a734e75bbfdff6dbec362fe32d8cb3b722f9da
Your Name <you@example.com>
2025-12-23 19:47:23 +0600

    first commit

---
+++ Hello.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World");
+    // printf("How are you! Bro Kemon Asen!");
+    int number = 25;
+    char dollar = "$";
+    int age = 20;
+    age = 22;
+    float pi = 3.14;
+    // variable ar case sensetive
+     int a = 30;
+     int A = 40;
+     
+    //  you  can start write variable only use alpabet and _
+    // first
+    int _age = 22;
+    // !second
+    int start = "string";
+    // varable maje kono khali jaiga rakha jabe na
+    // tobe mjkhane _ (hig pen use korte parbe
+    int final_price = 1000;
+    return 0;
+}
+
+// !Variables
+// !
+// !Rules
+// !
+// !a. Variables are case sensitive
+// !
+// !b. 1st character is alphabet or '_'
+// !
+// !c. no comma/blank space
+// !
+// !d. No other symbol other than '_'
+
+
+
+
+
+// ?Variables
+// ?Data Types

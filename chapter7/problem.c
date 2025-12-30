1d9d3a77b253ef63e729d27a9e96ba63e96ed108
Your Name <you@example.com>
2025-12-30 11:10:01 +0600

    complete chapter 7

--- chapter7/problem.c
+++ chapter7/problem.c
@@ -1,7 +1,29 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-int main(){
+int main()
+{
 
+    /*
+    !! Queston 41
+    *Write a program to enter price of 3 items & print their final cost with gst
+
+    */
+
+    float items[3];
     
+    printf("Enter items 1 : ");
+    scanf("%f" , &items[0]);
+
+    printf("Enter items 2 : ");
+    scanf("%f" , &items[1]);
+
+    printf("Enter items 3 : ");
+    scanf("%f" , &items[2]);
+
+    printf("Total price 1 : %f \n" , items[0]+(0.18*items[0]));
+    printf("Total price 2 : %f \n" , items[1]+(0.18*items[1]));
+    printf("Total price 3 : %f \n" , items[2]+(0.18*items[2]));
+
+    return 0;
 }
